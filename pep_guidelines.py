69583a486bd8fdff6eb71b8f42ec3a0a988c7620
Tarun Brari <hellknight@turing.teslanet>
2018-03-15 02:14:41 +0530

    new file Pep added

---
+++ pep_guidelines.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from MyModule import foo, bar, foobar
+
+
+def my_function(one, two, 
+                three, four, 
+                five, six):
+    print("Hello World")
+
+def second_fuction():
+    print("Second Function")
+
+my_list = [1, 2, 
+           3, 4,
+           5, 6]
+
+print("Hello ", "Hi")
+
+x = 3 * 52 + 7
+
+check = True
+if check is True:
+    print("This is true")
+
+func_one()
+func_two()
+func_three()
