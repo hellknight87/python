2d5ef747ac6b0576d1d9c4f91b93f01d91992a30
Tarun Brari <hellknight@turing.teslanet>
2018-03-15 03:10:40 +0530

    Added hp = 200 for all the enemies in the class

--- attack.py
+++ attack.py
@@ -1,6 +1,7 @@
 import random
 
 class Enemy:
+    hp = 200
 
     def __init__(self, atkl, atkh):
         self.atkl = atkl
@@ -8,13 +9,18 @@ class Enemy:
 
 
     def getAtk(self):
-        print(self.atkl)
+        print("atk is ", self.atkl)
+    
+    def getHp(self):
+        print("Hp is ", self.hp)
 
 enemy1 = Enemy(40, 49)
 enemy1.getAtk()
+enemy1.getHp()
 
 enemy2 = Enemy(75, 90)
 enemy2.getAtk()
+enemy2.getHp()
 
 '''
 
