ddcbec1fb3df41aeacde26b0ffeee2979c5b6706
hellknight87 <37348009+hellknight87@users.noreply.github.com>
2018-03-15 05:59:54 +0530

    Added laptop.py with system information.

---
+++ laptop.py
@@ -0,0 +1,6 @@
+#This is the information about laptop's configuration
+print("HP 15 Laptop")
+print("Intel Core i3 6006u")
+print("4 GB DDR4L RAM @ 2400 MHz")
+print("120 GB Western Digital Blue SSD")
+print("15.6 inch screen @ 1366 x 768")
