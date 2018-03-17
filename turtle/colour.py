ee107e3afd61ecd6f891ba82fcdcda2cdf51755f
Tarun Brari <hellknight@turing.teslanet>
2018-03-17 17:13:31 +0530

    Increased the speed of the turtle

--- turtle/colour.py
+++ turtle/colour.py
@@ -3,21 +3,23 @@ import turtle
 
 painter = turtle.Turtle()
 
+painter.speed(100)
+
 painter.pencolor("blue")
 
-for i in range(50):
+for i in range(100):
     painter.forward(50)
     painter.left(123) #counter clockwise
 
 painter.pencolor("red")
 
-for i in range(50):
+for i in range(100):
     painter.forward(100)
     painter.left(123)
 
 painter.pencolor("green")
 
-for i in range(50):
+for i in range(100):
     painter.forward(150)
     painter.left(123)
 
